6fb198f5086bcc4f12d90a5a629123dfc78aef10
igorWebMASTER <igor_1917@hotmail.com>
2020-10-06 18:41:13 -0300

    Conecção com mongo ok

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import colors from 'colors';
 import products from './data/products.js';
 
 dotenv.config();
@@ -26,5 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow)
 );
