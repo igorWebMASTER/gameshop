c21eb504ffa9eff0dd332dc92896d08d9a0be445
igorWebMASTER <igor_1917@hotmail.com>
2020-10-19 22:17:07 -0300

    Finalizar pedido adicionado com requisição ao Mongo

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   Form,
   Button,
@@ -11,9 +11,13 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckOutSteps from '../components/CheckOutSteps';
+import { createOrder } from '../actions/orderActions';
+
 import { Link } from 'react-router-dom';
 
 const PlaceOrder = ({ history }) => {
+  const dispatch = useDispatch();
+
   const cart = useSelector((state) => state.cart);
 
   // Calculate prices
@@ -35,8 +39,28 @@ const PlaceOrder = ({ history }) => {
     Number(cart.taxPrice)
   ).toFixed(2);
 
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+    }
+    // eslint-disable-next-line
+  }, [history, success]);
+
   const placeOrderHandler = () => {
-    console.log('success');
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
@@ -90,7 +114,7 @@ const PlaceOrder = ({ history }) => {
                         </Col>
 
                         <Col md={5}>
-                          {item.qty} x R${item.price} = ${item.qty} * R$
+                          {item.qty} x R${item.price} = {item.qty} * R$
                           {item.price}
                         </Col>
                       </Row>
@@ -130,6 +154,9 @@ const PlaceOrder = ({ history }) => {
                 <Col>Total</Col>
                 <Col>R$ {cart.totalPrice}</Col>
               </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              {error && <Message variant='danger'>{error}</Message>}
             </ListGroup.Item>
             <ListGroup.Item>
               <Button
