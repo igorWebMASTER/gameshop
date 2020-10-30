586e84d8fc0342258e0d31be8f60c30ef537922d
igorWebMASTER <igor_1917@hotmail.com>
2020-10-30 01:09:59 -0300

    Lista de pedidos no admin adicionado

---
+++ frontend/src/screens/OrderListScreen.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { Table, Row, Col, Image, Button } from 'react-bootstrap';
+
+import { listOrders } from '../actions/orderActions';
+
+const OrderListScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history, userInfo]);
+
+  return (
+    <>
+      <div>
+        <h1>Pedidos:</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <td>ID</td>
+                <td>USER</td>
+                <td>DATA</td>
+                <td>TOTAL</td>
+                <td>PAID</td>
+                <td>ENTREGA</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </thead>
+
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.user && order.user.name}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>R$ {order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fa fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fa fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button variant='light' className='btn-sm'>
+                        Detalhes
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default OrderListScreen;
