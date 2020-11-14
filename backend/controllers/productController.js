f14b2072580cdacd80a0c02ffa1547d15f37699e
igorWebMASTER <igor_1917@hotmail.com>
2020-11-14 17:32:40 -0300

    Carousel finalizado

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,11 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-// @dec Fetch all products
-// @route GET /api/products
-// @access Public
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2;
+  const pageSize = 5;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
@@ -149,6 +149,15 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @dec Get Top rated products
+// @route GET /api/products/top
+// @access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductsById,
@@ -156,4 +165,5 @@ export {
   updateProduct,
   createProduct,
   createProductReview,
+  getTopProducts,
 };
--- frontend/src/screens/HomesScreen.js
+++ frontend/src/screens/HomesScreen.js
@@ -3,8 +3,10 @@ import { Row, Col, Spinner } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { listProducts } from '../actions/productActions';
 import Message from '../components/Message';
+import ProductCarousel from '../components/ProductCarousel';
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
@@ -14,7 +16,7 @@ const HomeScreen = ({ match }) => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, page, pages } = productList;
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber));
@@ -22,20 +24,28 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
+      {!keyword && <ProductCarousel />}
       <h1>Últimos produtos</h1>
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant='danger'>{error}</Message>
-        ) : (
-          products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))
-        )}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
+      )}
     </>
   );
 };
